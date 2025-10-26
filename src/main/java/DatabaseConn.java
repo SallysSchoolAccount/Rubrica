e03f6f81b2878aafe0310f70c0887f83acb7faaa
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-26 14:53:13 +0100

    create connection (UNTESTED)

--- src/main/java/DatabaseConn.java
+++ src/main/java/DatabaseConn.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConn {
 
-    private static String url = "";
+    private static String url = "jdbc:sqlite:./Database/Phonebook.db";
+    private static String user = "";
+    private static String password = "";
 
+    private DatabaseConn() {
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection(url, user, password);
+        return conn;
+    }
 }
