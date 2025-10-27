2fcc14aaebb8401f3d3bc75eab162df54fd53114
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-27 14:05:50 +0100

    add getAll()

--- src/main/java/ContactDAOImpl.java
+++ src/main/java/ContactDAOImpl.java
@@ -1,5 +1,9 @@
 import DAO.ContactDAO;
 import Models.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -29,5 +33,28 @@ public class ContactDAOImpl implements ContactDAO {
             contact = new Contact(vorname, nachname, telefonnummer, email);
         }
         return contact;
+    }
+    //GET ALL
+    @Override
+    public java.util.List<Contact> getAll() throws SQLException {
+        Connection conn  =  DatabaseConn.getConnection();
+        List<Contact> contacts = new ArrayList<>();
+
+        String sql = "SELECT id, vorname, nachname, telefonnummer, email FROM t_contacts";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            int oid = rs.getInt("id");
+            String vorname = rs.getString("vorname");
+            String nachname = rs.getString("nachname");
+            String telefonnummer = rs.getString("telefonnummer");
+            String email = rs.getString("email");
+
+            Contact contact = new Contact(vorname, nachname, telefonnummer, email);
+            contacts.add(contact);
+        }
+
+        return contacts;
     }
 }
