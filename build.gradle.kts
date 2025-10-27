c55475b2e5a5ecceabd0fba8add61fcdc9079404
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-27 14:07:01 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -9,13 +10,23 @@ repositories {
     mavenCentral()
 }
 
+
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.xerial:sqlite-jdbc:3.x.x")
+    implementation("org.xerial:sqlite-jdbc:3.42.0.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    jvmArgs("--enable-native-access=ALL-UNNAMED")
+
 }
