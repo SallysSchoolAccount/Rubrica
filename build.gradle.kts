509d0ddc4df9a259eedaa41565baee7570bb3f69
Sal_ <salvatore.pontillo@stud.edubs.ch>
2025-10-28 13:19:14 +0100

    add javaFX to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -10,12 +11,15 @@ repositories {
     mavenCentral()
 }
 
-
-
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+}
+
+javafx {
+    version = "23.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml")
 }
 
 dependencies {
@@ -23,6 +27,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.xerial:sqlite-jdbc:3.42.0.0")
+}
+
+application {
+    mainClass.set("Main")
+    applicationDefaultJvmArgs = listOf(
+        "--enable-native-access=ALL-UNNAMED"
+    )
 }
 
 tasks.test {
