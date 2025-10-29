ed4642b9f376a6b6acc28771bbc5f2d7f65b4be1
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-29 14:48:27 +0100

    fix createPfp()

--- src/main/java/UI/contactBox.java
+++ src/main/java/UI/contactBox.java
@@ -13,8 +13,9 @@ public class contactBox {
     Contact contact = new Contact(1, "Mario", "Rossi", "0123456789", "");
 
     public Node createContactBox() {
-        HBox hBox = new HBox(40, createName());
+        HBox hBox = new HBox(40, createPfp(), createName());
         VBox vBox = new VBox(40, hBox);
+        vBox.setPadding(new Insets(10, 10, 10, 10));
         return vBox;
     }
 
@@ -22,14 +23,14 @@ public class contactBox {
         String vorname = contact.getVorname();
         String nachname = contact.getNachname();
         Label result = new Label(vorname + " " + nachname);
-        result.setPadding(new Insets(30, 10, 0, 20));
+        result.setPadding(new Insets(10, 0, 0, 0));
         return result;
     }
     private Node createPfp() {
-        Image image = new Image("profilePicture.jpg");
+        Image image = new Image(getClass().getClassLoader().getResourceAsStream("profilePicture.png"));
         ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(24);
-        imageView.setFitWidth(24);
+        imageView.setFitHeight(50);
+        imageView.setFitWidth(50);
         imageView.setPreserveRatio(true);
         imageView.setSmooth(true);
         return imageView;
