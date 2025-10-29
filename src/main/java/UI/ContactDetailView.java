5df9612c0829e5e1171099ed95a128427e3a56fb
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-29 21:09:01 +0100

    initialise ContactDetailView

---
+++ src/main/java/UI/ContactDetailView.java
@@ -0,0 +1,16 @@
+package UI;
+
+import Models.Contact;
+import javafx.scene.Node;
+
+public class ContactDetailView {
+    private final Contact contact;
+
+    public ContactDetailView(Contact contact) {
+        this.contact = contact;
+    }
+
+    private Node createDetailView() {
+        return null;
+    }
+}
