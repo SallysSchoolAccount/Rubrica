071548d34db29d378cfffca14bc132212de151d3
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-29 22:12:42 +0100

    create and implement profilePicture

--- src/main/java/UI/profilePicture.java
+++ src/main/java/UI/profilePicture.java
@@ -1,10 +1,17 @@
 package UI;
 
 import javafx.scene.Node;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public record profilePicture() {
-    public Node profilePicture(double width, double height) {
+    public Node createPfp(double width, double height) {
+        Image image = new Image(getClass().getClassLoader().getResourceAsStream("profilePicture.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(height);
+        imageView.setFitWidth(width);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
         return null;
     }
 }
