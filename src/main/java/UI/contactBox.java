a86b88e169be98e432a9d114235393d160d66725
Sal_ <salvatore.pontillo@stud.edubs.ch>
2025-10-29 17:21:19 +0100

    made contactBox clickable

--- src/main/java/UI/contactBox.java
+++ src/main/java/UI/contactBox.java
@@ -2,12 +2,15 @@ package UI;
 
 import Models.Contact;
 import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
 
 public class contactBox {
     Contact contact = new Contact(1, "Mario", "Rossi", "0123456789", "");
@@ -16,17 +19,22 @@ public class contactBox {
         HBox hBox = new HBox(40, createPfp(), createName());
         VBox vBox = new VBox(40, hBox);
         vBox.setPadding(new Insets(10, 10, 10, 10));
-        vBox.getStyleClass().add("contact-box");
+        vBox.getStyleClass().add("contact-box"); //Stylesheet
+
+//      Stuff for clickable
+        vBox.setCursor(Cursor.HAND);
+        vBox.setPickOnBounds(true);
+        vBox.setOnMouseClicked(evt -> System.out.println("Clicked: " + contact.getVorname() + " " + contact.getNachname()));
         return vBox;
     }
 
     private Node createName() {
         String vorname = contact.getVorname();
         String nachname = contact.getNachname();
-        Label result = new Label(vorname + " " + nachname);
-        result.setPadding(new Insets(10, 0, 0, 0));
-        result.getStyleClass().add("contact-name");
-        return result;
+        Label labelFullname = new Label(vorname + " " + nachname);
+        labelFullname.setPadding(new Insets(10, 0, 0, 0));
+        labelFullname.getStyleClass().add("contact-name");//Stylesheet
+        return labelFullname;
     }
     private Node createPfp() {
         Image image = new Image(getClass().getClassLoader().getResourceAsStream("profilePicture.png"));
