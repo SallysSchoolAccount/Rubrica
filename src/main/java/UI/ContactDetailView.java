ada4c097a10513bdc964e3f77fbaef675b80cfa0
Salvatore <salvatore.pontillo@stud.edubs.ch>
2025-10-30 20:05:08 +0100

    update ContactDetailView added an edit mode, now its possible to edit contacts. (not persistent in Database)

--- src/main/java/UI/ContactDetailView.java
+++ src/main/java/UI/ContactDetailView.java
@@ -4,25 +4,37 @@ import Models.Contact;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 public class ContactDetailView {
     private final Contact contact;
+    private boolean isEditing = false;
+    private VBox vBox;
+
+    private TextField vornameField;
+    private TextField nachnameField;
+    private TextField telefonnummerField;
+    private TextField emailField;
 
     public ContactDetailView(Contact contact) {
         this.contact = contact;
     }
 
     public Node createDetailView() {
-        VBox vBox = new VBox();
-        vBox.setPadding(new Insets(30));
-        vBox.setAlignment(Pos.TOP_CENTER);
-        vBox.getStyleClass().add("contact-detail");
+        if (vBox == null) {
+            vBox = new VBox();
+            vBox.setPadding(new Insets(30));
+            vBox.setAlignment(Pos.TOP_CENTER);
+            vBox.getStyleClass().add("contact-detail");
+        } else {
+            vBox.getChildren().clear();
+        }
 
         uiElements pfp = new uiElements();
 
@@ -30,7 +42,6 @@ public class ContactDetailView {
         Label nameLabel = new Label(contact.getVorname() + " " + contact.getNachname());
         nameLabel.setFont(Font.font("System", FontWeight.BOLD, 24));
 
-        // Contact details
         VBox infoBox = new VBox(10);
         infoBox.setAlignment(Pos.CENTER_LEFT);
         infoBox.setPadding(new Insets(20, 0, 0, 0));
@@ -43,8 +54,101 @@ public class ContactDetailView {
 
         infoBox.getChildren().addAll(phoneLabel, emailLabel);
 
-        vBox.getChildren().addAll(pfp.createPfp(150, 150), nameLabel, infoBox);
+        // Edit button
+        Button editButton = new Button("Edit");
+        editButton.setFont(Font.font(14));
+        editButton.setPadding(new Insets(10, 20, 10, 20));
+        editButton.setOnAction(e -> toggleEditMode());
+
+        vBox.getChildren().addAll(pfp.createPfp(150, 150), nameLabel, infoBox, editButton);
 
         return vBox;
     }
+
+    private void toggleEditMode() {
+        if (!isEditing) {
+            showEditMode();
+        } else {
+            saveChanges();
+            createDetailView();
+        }
+        isEditing = !isEditing;
+    }
+
+    private void showEditMode() {
+        vBox.getChildren().clear();
+
+        uiElements pfp = new uiElements();
+        vBox.getChildren().add(pfp.createPfp(150, 150));
+
+        VBox editBox = new VBox(15);
+        editBox.setAlignment(Pos.CENTER_LEFT);
+        editBox.setPadding(new Insets(20, 0, 0, 0));
+
+        vornameField = new TextField(contact.getVorname());
+        HBox vornameBox = createEditField("First Name:", vornameField);
+
+        nachnameField = new TextField(contact.getNachname());
+        HBox nachnameBox = createEditField("Last Name:", nachnameField);
+
+        telefonnummerField = new TextField(contact.getTelefonNumer());
+        HBox phoneBox = createEditField("Phone:", telefonnummerField);
+
+        emailField = new TextField(contact.getEmail());
+        HBox emailBox = createEditField("Email:", emailField);
+
+        editBox.getChildren().addAll(vornameBox, nachnameBox, phoneBox, emailBox);
+
+        // Buttons
+        HBox buttonBox = new HBox(10);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.setPadding(new Insets(10, 0, 0, 0));
+
+        Button saveButton = new Button("Save");
+        saveButton.setFont(Font.font(14));
+        saveButton.setPadding(new Insets(10, 20, 10, 20));
+        saveButton.setOnAction(e -> {
+            saveChanges();
+            createDetailView();
+            isEditing = false;
+        });
+
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setFont(Font.font(14));
+        cancelButton.setPadding(new Insets(10, 20, 10, 20));
+        cancelButton.setOnAction(e -> {
+            createDetailView();
+            isEditing = false;
+        });
+
+        buttonBox.getChildren().addAll(saveButton, cancelButton);
+
+        vBox.getChildren().addAll(editBox, buttonBox);
+    }
+
+    private HBox createEditField(String labelText, TextField textField) {
+        HBox box = new HBox(10);
+        box.setAlignment(Pos.CENTER_LEFT);
+
+        Label label = new Label(labelText);
+        label.setFont(Font.font(14));
+        label.setPrefWidth(100);
+
+        textField.setPrefWidth(200);
+
+        box.getChildren().addAll(label, textField);
+        return box;
+    }
+
+    private void saveChanges() {
+        contact.setVorname(vornameField.getText());
+        contact.setNachname(nachnameField.getText());
+        contact.setTelefonNumer(telefonnummerField.getText());
+        contact.setEmail(emailField.getText());
+
+        // TODO: Save to database using ContactDAO
+        // ContactDAOImpl contactDAO = new ContactDAOImpl();
+        // contactDAO.update(contact);
+    }
+
 }
